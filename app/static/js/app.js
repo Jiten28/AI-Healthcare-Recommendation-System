39907f1ae542f16ea4d587034650b4c7bd97c7de
Jiten Kumar <77165984+Jiten28@users.noreply.github.com>
2026-07-04 01:35:06 +0530

    Complete landing page hero section and KPI statistics cards

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,72 @@
-console.log("MediVerse AI Loaded 🚀");
+// Navbar Scroll
+window.addEventListener("scroll", () => {
+
+    const navbar = document.getElementById("mainNavbar");
+
+    if (window.scrollY > 40) {
+
+        navbar.classList.add("scrolled");
+
+    } else {
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
+
+// Hero Animation
+
+window.onload = () => {
+
+    gsap.from("h1",{
+
+        y:60,
+
+        opacity:0,
+
+        duration:1
+
+    });
+
+    gsap.from(".lead",{
+
+        y:40,
+
+        opacity:0,
+
+        duration:1,
+
+        delay:.4
+
+    });
+
+    gsap.from(".btn",{
+
+        y:30,
+
+        opacity:0,
+
+        duration:1,
+
+        delay:.7,
+
+        stagger:.2
+
+    });
+
+    gsap.from("#heroLogo",{
+
+        scale:.5,
+
+        opacity:0,
+
+        rotation:20,
+
+        duration:1.4,
+
+        ease:"back.out(1.7)"
+
+    });
+
+};
